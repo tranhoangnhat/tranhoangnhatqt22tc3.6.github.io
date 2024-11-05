fd8eaf9dc2b27711195a66e9d1ec2486f64fedf7
tranhoangnhat <tranhoangnhatqt22tc3.6@gmail.com>
2024-11-05 13:48:39 +0700

    Add files via upload

---
+++ baiwebtudo.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Danh sách nhân vật trong conan</title>
+<link rel="stylesheet" href="kh.css">
+</head>
+<body>
+<header>
+	<h1 align="center">Thám tử lừng danh</h1>
+	<div class="top-bar"></div>
+	<nav class="menu-bar">
+	<ul>
+		<li><a href="#">TRANG CHỦ</a></li>
+		<li><a href="#">THÀNH VIÊN</a></li>
+		<li><a href="#">LIÊN HỆ</a></li>
+	
+		</ul>
+	</nav>
+</header>
+<div class="container">
+	<div class="header-row">
+		<h2 class="left">Thông tin nhân vật</h2>
+	<from class="search-from" method="GET">
+		<input type="text" name="search" placeholder="Tìm kiếm nhân vật...">
+		<button type="submit">Tìm kiếm</button>
+	</from>
+</div>
+	<img src="file:///C:/Users/DHA/Downloads/t%E1%BA%A3i%20xu%E1%BB%91ng%20(2).jfif" alt="Conan Edogawa" width="182" height="211">
+	<h3 class="right">Conan Edogawa
+Tuổi: 
+6 – 7 tuổi (Từ tập 1 đến tập 434)
+8 tuổi (từ tập 435)
+Giới tính: Nam
+Chiều cao: 95cm
+Cân nặng: 18kg
+Ngày sinh: 04/05/1983
+Người thân: Bố: Kudo Yusaku; Mẹ: Kudo Yukiko
+Nghề nghiệp: Thành viên Đội Thám tử nhí – Học sinh trường tiểu học
+</h3>
+	<br><img src="file:///C:/Users/DHA/Downloads/t%E1%BA%A3i%20xu%E1%BB%91ng%20(1).jfif" alt="">
+	<h3 class="right"</h3>Tên tiếng Nhật:	毛利 小五郎
+(Mōri Kogorō)
+Tên tiếng Anh:	Richard Moore
+Tuổi:	38[1]
+Giới tính:	Nam
+Ngày sinh:	Ngày 22 Tháng Năm
+Người thân:	Ran Mouri (con gái)
+Eri Kisaki (vợ)
+Nghề nghiệp:	Thám tử tư
+Cảnh sát (đã giải nghệ)
+Biệt danh:	Kogoro ngủ gật
+Shinigami (Thần Chết)
+	<div class="add-button">
+	</div>
+	</div>
+	<footer>
+	<p>Địa chỉ: Conan</p>
+	<p>Số điện thoại: 19001011</p>
+	</footer>
+</body>
+</html>
